18f32945a3a0504ee006b96774756a7ef7aa5b61
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-24 15:49:01 +0300

    Add confirmable module to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :transactions, class_name: 'Transaction', foreign_key: 'author_id'
   has_many :categories, class_name: 'Category', foreign_key: 'author_id'
 end
