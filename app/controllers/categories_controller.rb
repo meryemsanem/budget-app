e26771d44021d9b8dbc035e6f044f4efb12a8b5d
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-24 19:47:29 +0300

    Fix linter errors

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!  
+  before_action :authenticate_user!
   def index
-    @categories = Category.where(author: current_user) 
+    @categories = Category.where(author: current_user)
   end
 
   def new
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-   has_many :transaction_categories
+  has_many :transaction_categories
   has_many :categories, through: :transaction_categories
 
   validates :name, presence: true, length: { maximum: 255 }
