b736f2bcecd40e667ca928e68b09a65f703c0499
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-24 18:24:49 +0300

    Implement CategoriesController for index, new, and create actions.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,26 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!  
+  def index
+    @categories = Category.where(author: current_user) 
+  end
+
+  def new
+    @category = current_user.categories.build
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully saved.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
