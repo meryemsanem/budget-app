f16b3df1625defb489b6d19b0fe1d3116e487632
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-24 15:38:12 +0300

    Fix linter errors

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :transaction_categories
   has_many :transactions, through: :transaction_categories
-  
+
   validates :name, presence: true
   validates :icon, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :transaction_categories
   has_many :categories, through: :transaction_categories
-  
+
   validates :name, presence: true, length: { maximum: 255 }
-  validates :amount, presence: true, numericality: { greater_than: 0, only_integer: true}
+  validates :amount, presence: true, numericality: { greater_than: 0, only_integer: true }
 end
