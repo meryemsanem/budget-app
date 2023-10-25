56a28ecb61b7245afe0fe0cce46d56c66ec8bfcf
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-25 15:18:10 +0300

    Add tests for categories index and new views

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
