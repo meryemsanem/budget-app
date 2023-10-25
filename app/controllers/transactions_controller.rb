bb2adab78440eb2d480214aafb0b1d3875d6f9ea
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-25 12:19:03 +0300

    Create TransactionsController with index, new, and create actions

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,29 @@
+class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @category = Category.find(params[:category_id])
+    @transactions = @category.transactions.where(author_id: current_user.id).order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.find(params[:category_id])
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = current_user.transactions.new(transaction_params)
+     @transaction.user_id = current_user.id
+    if @transaction.save
+      redirect_to category_transactions_path(transaction_params[:category_id]),
+                  notice: 'Transaction was successfully saved.'
+    else
+      render :new
+    end
+  end
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :amount, :category_id)
+  end
+end
